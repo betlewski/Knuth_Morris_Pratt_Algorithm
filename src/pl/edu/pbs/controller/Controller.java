1b712eddf854110521e0c403dbb6d92ab4e6e188
mbalcer <mateusz75319@gmail.com>
2022-01-14 19:40:46 +0100

    Created view and Controller

---
+++ src/pl/edu/pbs/controller/Controller.java
@@ -0,0 +1,20 @@
+package pl.edu.pbs.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class Controller {
+    @FXML
+    private TextField pathToFileLabel;
+    @FXML
+    private TextField searchPatternLabel;
+    @FXML
+    private TextField resultsLabel;
+    @FXML
+    private Button searchBtn;
+
+    public void initialize() {
+
+    }
+}
